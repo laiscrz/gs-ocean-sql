f361595103be40b18a430b4252df918c41f84232
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-05-28 16:42:37 -0300

    Create codigoGS.sql

---
+++ codigoGS.sql
@@ -0,0 +1,9 @@
+/*
+Empresa LeadTech
+
+Bianca Leticia Román Caldeira - RM552267 - Turma : 2TDSPH
+Charlene Aparecida Estevam Mendes Fialho - RM552252 - Turma : 2TDSPH
+Lucca Raphael Pereira dos Santos - RM 99675 - Turma : 2TDSPW
+Lais Alves Da Silva Cruz - RM:552258 - Turma : 2TDSPH
+Fabrico Torres Antonio - RM:97916 - Turma : 2TDSPH
+*/
