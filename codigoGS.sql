016ae81693ed4e2108a85524dca40eb3fb5d0bcd
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-05-30 13:13:27 -0300

    add drops em codigoGS.sql

--- codigoGS.sql
+++ codigoGS.sql
@@ -3,13 +3,37 @@ Empresa LeadTech
 
 Bianca Leticia Román Caldeira - RM552267 - Turma : 2TDSPH
 Charlene Aparecida Estevam Mendes Fialho - RM552252 - Turma : 2TDSPH
+Lais Alves Da Silva Cruz - RM 552258 - Turma : 2TDSPH
 Lucca Raphael Pereira dos Santos - RM 99675 - Turma : 2TDSPW
-Lais Alves Da Silva Cruz - RM:552258 - Turma : 2TDSPH
-Fabrico Torres Antonio - RM:97916 - Turma : 2TDSPH
+Fabrico Torres Antonio - RM 97916 - Turma : 2TDSPH
 */
 
 -- DDL (DROP,CREATE, ALTER)
+-- Comandos para definição de dados, incluindo a remoção de tabelas existentes, criações, alterações e suas restrições.
+
+/* Remove a tabela 'categoria' e quaisquer restrições que dependem dela */
+DROP TABLE categoria CASCADE CONSTRAINTS;
+
+/* Remove a tabela 'deteccao' e quaisquer restrições que dependem dela */
+DROP TABLE deteccao CASCADE CONSTRAINTS;
+
+/* Remove a tabela 'especie' e quaisquer restrições que dependem dela */
+DROP TABLE especie CASCADE CONSTRAINTS;
+
+/* Remove a tabela 'localizacao' e quaisquer restrições que dependem dela */
+DROP TABLE localizacao CASCADE CONSTRAINTS;
+
+/* Remove a tabela 'ong' e quaisquer restrições que dependem dela */
+DROP TABLE ong CASCADE CONSTRAINTS;
+
+/* Remove a tabela 'situacao' e quaisquer restrições que dependem dela */
+DROP TABLE situacao CASCADE CONSTRAINTS;
+
+/* Remove a tabela 'usuario' e quaisquer restrições que dependem dela */
+DROP TABLE usuario CASCADE CONSTRAINTS;
+
 
 -- CARGA DE DADOS
+
 
 -- RELATÓRIOS
