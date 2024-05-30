38c4a5e899d5d4b86dd09acb04859f09a4a89d2f
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-05-30 14:24:49 -0300

    add creates tables em codigoGS.sql

--- codigoGS.sql
+++ codigoGS.sql
@@ -34,6 +34,62 @@ DROP TABLE situacao CASCADE CONSTRAINTS;
 DROP TABLE usuario CASCADE CONSTRAINTS;
 
 /* CREATES TABLES */
+CREATE TABLE usuario (
+    id_usuario NUMBER(8) PRIMARY KEY,
+    nome VARCHAR2(60),
+    email VARCHAR2(30),
+    senha VARCHAR2(20)
+);
+
+CREATE TABLE ong (
+    id_ong NUMBER(8) PRIMARY KEY,
+    cnpj VARCHAR2(14),
+    nome VARCHAR2(60),
+    email VARCHAR2(30),
+    telefone VARCHAR2(12)
+);
+
+CREATE TABLE localizacao (
+    id_localizacao NUMBER(8)PRIMARY KEY,
+    latitude NUMBER(9,6),
+    longitude NUMBER(9,6),
+    cidade VARCHAR2(40),
+    estado VARCHAR2(40),
+    pais VARCHAR2(40)
+);
+
+CREATE TABLE categoria (
+    id_categoria NUMBER(8) PRIMARY KEY,
+    nome VARCHAR2(60),
+    habitat VARCHAR2(30),
+    reino VARCHAR2(30),
+    familia VARCHAR2(30)
+);
+
+CREATE TABLE situacao (
+    id_situacao NUMBER(8) PRIMARY KEY,
+    risco_extincao CHAR(1),
+    invasora CHAR(1)
+);
+
+CREATE TABLE especie (
+    id_especie NUMBER(8) PRIMARY KEY,
+    nome_comum VARCHAR2(60),
+    nome_cientifico VARCHAR2(60),
+    descricao VARCHAR2(150),
+    situacao_id NUMBER(8) NOT NULL,
+    categoria_id NUMBER(8) NOT NULL
+);
+
+CREATE TABLE deteccao (
+    id_deteccao NUMBER(8) PRIMARY KEY,
+    url_imagem VARCHAR2(60),
+    data_deteccao DATE,
+    usuario_id NUMBER(8) NOT NULL,
+    especie_id NUMBER(8) NOT NULL,
+    ong_id NUMBER(8) NOT NULL,
+    localizacao_id NUMBER(8) NOT NULL
+);
 
 /* ALTERS TABLES */
 
